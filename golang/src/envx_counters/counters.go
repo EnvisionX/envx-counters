96acd69a8d718afb5d5b0bd49a01c66f087523e9
Aleksey Morarash <aleksey.morarash@envisionx.co>
2018-09-14 12:47:47 +0300

    Move server code to separate file

--- golang/src/envx_counters/counters.go
+++ golang/src/envx_counters/counters.go
@@ -10,19 +10,13 @@ Copyright: 2016, EnvisionX <info@envisionx.co>
 package envx_counters
 
 import (
-	"bufio"
 	"fmt"
-	"net"
 	"os"
-	"strconv"
-	"strings"
 	"sync"
 	"sync/atomic"
-	"time"
 )
 
 const (
-	DEFAULT_PORT = 8907
 	ENV_DISABLED = "ENVX_COUNTERS_DISABLED"
 	ENV_PORT     = "ENVX_COUNTERS_PORT"
 )
@@ -40,7 +34,7 @@ func init() {
 	gDisabled = len(os.Getenv(ENV_DISABLED)) > 0
 	if !gDisabled {
 		Initialize()
-		go tcp_srv()
+		go startServer()
 	}
 }
 
@@ -211,107 +205,4 @@ func List() []string {
 		i++
 	}
 	return res
-}
-
-// This thread accepts TCP connections from the network
-func tcp_srv() {
-	bindAddr := fmt.Sprintf(":%d", getPort())
-	for {
-		ss, err := net.Listen("tcp", bindAddr)
-		if err != nil {
-			// loop until success
-			time.Sleep(5 * time.Second)
-			continue
-		}
-		for {
-			socket, err := ss.Accept()
-			if err != nil {
-				time.Sleep(100 * time.Millisecond)
-				continue
-			}
-			go handleTcpConnection(socket)
-		}
-	}
-}
-
-// TCP connection handler thread
-func handleTcpConnection(socket net.Conn) {
-	scanner := bufio.NewScanner(socket)
-	for scanner.Scan() {
-		reply, err := processExtRequest(scanner.Text())
-		if err {
-			socket.Close()
-			return
-		}
-		if 0 < len(reply) {
-			socket.Write([]byte(reply))
-		}
-	}
-	socket.Close()
-}
-
-// Process request from external client.
-func processExtRequest(request string) (reply string, error bool) {
-	tokens := strings.Fields(request)
-	if len(tokens) < 1 {
-		return "", true
-	}
-	switch strings.ToUpper(tokens[0]) {
-	case "GET":
-		if len(tokens) != 2 {
-			return "", true
-		}
-		reply := fmt.Sprintln(tokens[1], Get(tokens[1]))
-		return reply, false
-	case "HIT":
-		if len(tokens) != 2 {
-			return "", true
-		}
-		Hit(tokens[1])
-		return "", false
-	case "SET":
-		if len(tokens) != 3 {
-			return "", true
-		}
-		value, err := strconv.ParseInt(tokens[2], 10, 64)
-		if err != nil {
-			return "", true
-		}
-		Set(tokens[1], value)
-		return "", false
-	case "LIST":
-		if len(tokens) != 1 {
-			return "", true
-		}
-		return strings.Join(List(), " ") + "\n", false
-	case "DUMP":
-		if len(tokens) != 1 {
-			return "", true
-		}
-		reply := ""
-		for _, name := range List() {
-			reply += fmt.Sprintf("%s %d\n", name, Get(name))
-		}
-		if len(reply) == 0 {
-			reply = "\n"
-		}
-		return reply, false
-	case "RESET":
-		if len(tokens) != 1 {
-			return "", true
-		}
-		Reset()
-		return "\n", false
-	}
-	return "", true
-}
-
-// Return TCP port number to be listened by servers.
-func getPort() uint16 {
-	strPort := os.Getenv(ENV_PORT)
-	port, err := strconv.ParseUint(strPort, 10, 16)
-	if err != nil {
-		port = DEFAULT_PORT
-	}
-	return uint16(port)
 }
